6721dc0f94c35a7fb723537f054d0ac587208b9c
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-19 17:21:32 -0500

    added instance method called add which adds a new body to the lost of bodies.

--- solarsystemtake2.rb
+++ solarsystemtake2.rb
@@ -4,4 +4,9 @@ class System
   def initialize
     @bodies=[]
   end
+
+  #give System an instance method called add which will add a celestial body to the list.
+  def add(body)
+    @bodies<<body
+  end
 end
