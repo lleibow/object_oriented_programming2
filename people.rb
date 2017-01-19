6ad711966cd5c3b4ac2b5ad7afee38d394cbb70f
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-19 13:21:52 -0500

    changed greeting method so that it has to be called on new instances. Used super to refer to parent method.

--- people.rb
+++ people.rb
@@ -1,7 +1,6 @@
 class Person
   def initialize(name)
     @name=name
-    greeting
   end
 
 def greeting
@@ -15,17 +14,25 @@ class Student < Person
   def learn
     puts "I get it"
   end
+
+  def greeting
+    super
+  end
+
 end
 
 class Instructor < Person
   def teach
     puts "Everything in Rucy is an Object"
   end
+
+  def greeting
+    super
+  end
 end
 
 #Practise running greeting
 laura=Person.new("Laura")
-laura.greeting
 
 #Practise running greeting on Instructor chris
 chris=Instructor.new("Chris")
